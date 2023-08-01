53d9d8e4f806b3137d8c0a701b7cc31c0431b95c
ajsuaa <22391@wc.school.nz>
2023-08-01 14:20:20 +1200

    1/08/23
    Camera and player movement added

---
+++ DTM12 - Staying Frosty (Project 2)/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The camera's X and Z axis change depending on the player's position in order to follow, while the Y axis of the camera remains the same
+        transform.position = player.transform.position + new Vector3(0, 19, 0);
+    }
+}
+
+
---
+++ DTM12 - Staying Frosty (Project 2)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Important - This allows the buttons to transfer the user to different scenes
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    //Determines the speed of the player
+    private float speed = 20.0f;
+
+    //Provides the  Rigidbody a variable
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    //Moves the player based on arrow key input
+    void MovePlayer()
+    {
+        //How the player is controlled (going horizontally and vertically,  move, left, up, down)
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //Applying the speed when controlling the player
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+
+    //This variable is used when recieving a Game Over
+    private void OnCollisionEnter(Collision other)
+    {
+        // When player collides with red walls, transfer user to Build Scene 2 (Game Over Scene)
+        if (other.gameObject.name == "Wall")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
