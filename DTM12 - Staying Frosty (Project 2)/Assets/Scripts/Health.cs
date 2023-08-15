e96301a71ae88ed4c7fd378cbc537547ac7713de
ajsuaa <22391@wc.school.nz>
2023-08-15 14:21:27 +1200

    15/08/23
    UI

--- DTM12 - Staying Frosty (Project 2)/Assets/Scripts/Health.cs
+++ DTM12 - Staying Frosty (Project 2)/Assets/Scripts/Health.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
+    public TMP_Text healthText;
+    public Image healthBar;
+
+    float health, maxHealth = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthText.text = "Health: " + health + "%";
+        //This makes sure that the health isn't greater than the maximum health
+        if (health > maxHealth) health = maxHealth;
+
+        HealthBarFiller();
+    }
+
+    void HealthBarFiller()
+    {
+        healthBar.fillAmount = health / maxHealth; 
+    }
+
+    public void Damage(float damagePoints)
+    {
+        if (health > 0)
+            health -= damagePoints;
+    }
+
+    public void Heal(float healingPoints)
+    {
+        if (health < maxHealth)
+            health += healingPoints;
     }
 }
 
