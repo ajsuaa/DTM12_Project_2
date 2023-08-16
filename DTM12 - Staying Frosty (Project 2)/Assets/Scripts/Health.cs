a1cb0129b12ae841e97209253c9532bbff64a2a7
ajsuaa <22391@wc.school.nz>
2023-08-17 10:10:29 +1200

    17/08/23
    Health Adjustments

--- DTM12 - Staying Frosty (Project 2)/Assets/Scripts/Health.cs
+++ DTM12 - Staying Frosty (Project 2)/Assets/Scripts/Health.cs
@@ -1,15 +1,28 @@
+//THIS SCRIPT IS FROM A SCRIPT
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Health : MonoBehaviour
 {
+    //This displays the Health Text (The text above the health bar)
     public TMP_Text healthText;
+    //This displays the Health Bar
     public Image healthBar;
 
+    //Health
+    float currentHealth= 0f;
+
+    //Shows the maximum amount of health
     float health, maxHealth = 100;
+    //This makes the health UI smoother
+    float lerpSpeed;
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,23 +38,44 @@ public class Health : MonoBehaviour
         if (health > maxHealth) health = maxHealth;
 
         HealthBarFiller();
+        ColorChanger();
+
+        if (health <= 0)
+        {
+            currentHealth = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+
     }
 
     void HealthBarFiller()
     {
-        healthBar.fillAmount = health / maxHealth; 
+        //This allows the animaton for the health bar decreasing and increasing a lot more smoother
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health / maxHealth, lerpSpeed);
     }
+
+    void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(Color.blue, Color.green, (health / maxHealth));
+        healthBar.color = healthColor;
+    }
+
+
 
     public void Damage(float damagePoints)
     {
-        if (health > 0)
+        //This lowers the health
+        if (health > 0) 
             health -= damagePoints;
     }
 
     public void Heal(float healingPoints)
     {
+        //This increases the health
         if (health < maxHealth)
             health += healingPoints;
     }
 }
+
+
 
